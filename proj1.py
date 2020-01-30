2e7b0e6a440cc321a76023329f38e93ab436d6ca
isaac.callisonSD <isaac.callisonsd@mtsu.edu>
2020-01-29 20:46:02 -0600

    added all comments

--- proj1.py
+++ proj1.py
@@ -3,7 +3,7 @@
 #CSCI 6350-001
 #Project #1
 #Due: 01/30/20
-
+# Please see answers to questions at the bottom of this code.
 import csv
 import random
 import numpy as np
@@ -12,18 +12,18 @@ def main():
     # hardcoded files to be imported
     leviDistance = "costs.csv"; confDistance = "costs2.csv"; wordList = "words.txt"
 
-    # object with 2d array wherein matices and word lists reside.
+    #matrix object with 2d array wherein matrices and word lists reside.
     global leviObj
     global confObj
     leviObj = matrixObj(openCSVFile(leviDistance))
     confObj = matrixObj(openCSVFile(confDistance))
     wordObjList = (openTXTFile(wordList))
 
-    for y, wObj in enumerate(wordObjList):
     ############ LOOP THROUGH ###################
+    for y, wObj in enumerate(wordObjList):
         for x in range(wObj.getListLeng()):
             n,m = wObj.getSrcWordLeng(x), wObj.getTrgtLeng()
-    # GET BACK intialized matrix.
+    # GET BACK intialized matrix for size and pad left and top.
             scrMatInit = scoreMatrixInit(n,m)
     # FINISH filling up the score matix. Change based on levinstein or confusion
             targetString = wObj.targetWord
@@ -41,26 +41,10 @@ def main():
             final_seq1, final_seq2, changes = reverseFinalStrings(final_seq1, final_seq2, final_seq2)
             printFinalOutput(final_seq1, final_seq2, cost,changes)
 
+            # Exactly 50 hyphens
             print("-"*50)
-
-# reverseFinalStrings just reverses the strings that were build duing the alighnment phase
-def reverseFinalStrings(final_seq1, final_seq2, changes):
-    changes = changes[::-1]
-    final_seq1 = final_seq1[::-1]
-    final_seq2 = final_seq2[::-1]
-    return final_seq1, final_seq2, changes
-
-# printFinalOutput outputs the final data to the screen
-def printFinalOutput(final_seq1, final_seq2, cost, changes):
-    sepLeng = len(final_seq1)
-    print(final_seq2)
-    print("|"*sepLeng)
-    print("{}".format(final_seq1))
-    print("{} ({})".format(changes, cost))
-    print()
-
 ######################### CLASSES ##########################
-
+## The matrixObj class is used for storing the Levenshtein and Confusion matrices
 class matrixObj:
     def __init__(self, array):
         self.array = array
@@ -71,7 +55,7 @@ class matrixObj:
     def getCost(self, i, j):
         return int(self.array[i][j])
 
-
+## The WordObj class is used to store the wordlist and extract the target and source words.
 class WordObj:
     def __init__(self, list):
         self.targetWord = list[0]
@@ -83,17 +67,16 @@ class WordObj:
     def getListLeng(self):
         return len(self.sourceList)
     def printList(self):
-        #print("Target word is {}".format(self.targetWord))
-        #print("Source Words are: ")
         for word in self.sourceList:
             print(word, end=" ")
         print()
 
 ####################FUNCTIONS###########################################################
+## findSequences uses the cost matrix to complete the minmum edit alignment and return the Target and Source strings.
+## It is still a little buggy.
 def findSequences(PMatrix, targetString, sourceString):
     TRG_SEQ = ""; SRC_SEQ = ""; changes = ""
     i = len(targetString); j = len(sourceString)
-    #print("target word {} and source word {}".format(targetString, sourceString))
 
     # navigate traceback array, start at bottom right.
     while i >= 0 or j >= 0:
@@ -104,7 +87,6 @@ def findSequences(PMatrix, targetString, sourceString):
         i -=1
         if i >=0:
             TRG_SEQ += targetString[i]
-            #print("TRG_SEQ {}".format(TRG_SEQ))
         ############ iterate through possible pointers ##############
         if rand_item == "\\":
             j-=1
@@ -127,8 +109,7 @@ def findSequences(PMatrix, targetString, sourceString):
 
         elif rand_item == "0":
             break
-
-    #print("i {} and j {}".format(i,j))
+    ## If we find ourselves and the top but not left, or at the far left, but not the top, we can finish iterating through i or j
     i-= 1; j -=1
     while i >= 0:
         SRC_SEQ += "*"
@@ -142,6 +123,24 @@ def findSequences(PMatrix, targetString, sourceString):
         j -= 1
     return TRG_SEQ,SRC_SEQ, changes
 
+
+# reverseFinalStrings just reverses the strings that were build duing the alignment phase.
+def reverseFinalStrings(final_seq1, final_seq2, changes):
+    changes = changes[::-1]
+    final_seq1 = final_seq1[::-1]
+    final_seq2 = final_seq2[::-1]
+    return final_seq1, final_seq2, changes
+
+# printFinalOutput outputs the final data to the screen
+def printFinalOutput(final_seq1, final_seq2, cost, changes):
+    sepLeng = len(final_seq1)
+    print(final_seq2)
+    print("|"*sepLeng)
+    print("{}".format(final_seq1))
+    print("{} ({})".format(changes, cost))
+    print()
+
+# fillScoreMatrix fills in the score matrix with the appropriate costs
 def fillScoreMatrix(n,m,scoreMat, targetString, sourceString, matrix_obj):
     n,m = n+1,m+1
     i,j = n,m
@@ -156,24 +155,28 @@ def fillScoreMatrix(n,m,scoreMat, targetString, sourceString, matrix_obj):
             pointer_list.append(pointer)
             PMatrix[i][j] = pointer
     cost = (scoreMat[n-1][m-1])
-
+    ############# Uncomment the following four lines to output the pointer matrix before random selection##################
     # for x in range(w):
     #     for y in range(h):
     #         print(PMatrix[x][y],end=" ")
     #     print()
+    ###############################################################################################
     return scoreMat, cost, PMatrix
 
+## return min returns the minimum cost as between the insertion, deletion, or substitution.
 def return_min(i, j, matrix, targetString, sourceString, matrix_obj):
-
+    ## Since the X and Y coords of the matricies are essentially in alphabetic order I used that to find the numneric value of that
+    ## char which also equaled the row and column allowing easy comparison of chars in the matricies.
     targetLetter = ord(targetString[j-1])-96
     sourceLetter = ord(sourceString[i-1])-96
+
     left = matrix[i-1,j] + 1 # delete cost of source(i)
     up = matrix[i,j-1] + 1 # ins cost of target(j)
     diag = matrix[i-1,j-1] + matrix_obj.getCost(sourceLetter,targetLetter)
+
     new_row = []
     new_row_dict ={"<":left,"^":up,"\\":diag}
     min_val = 1000
-    key_value = ""
     new_string = ""
     for k,v in new_row_dict.items():
         if v < min_val:
@@ -182,12 +185,11 @@ def return_min(i, j, matrix, targetString, sourceString, matrix_obj):
 
     for key, value in new_row_dict.items():
         if value == min_val:
-            #new_row.append(key)
             new_string+=key
-    #RAND_DIRECTION = new_row[random.randrange(0,len(new_row))]
-    #print("direction choosen {}".format(RAND_DIRECTION))
+    # new_string stores all the possible pointers.
     return min_val, new_string
 
+### scoreMatrixInit just creates a matrix of appropriate size and initializes some of the values.
 def scoreMatrixInit(n,m):
     n,m = n+1,m+1
     i,j = n,m
@@ -197,7 +199,7 @@ def scoreMatrixInit(n,m):
     for j in range(1,m):
         score_matrix[0,j] = score_matrix[0, j-1] + 1
     return score_matrix
-
+### openTXTFile opens the words file and creates a list of words objects wherein the target word can be utilized against the source words.
 def openTXTFile(file):
     listWordObjs = []
     f = open(file)
@@ -206,6 +208,7 @@ def openTXTFile(file):
         listWordObjs.append(WordObj(row))
     return listWordObjs
 
+### openCSVFile opens the CSV files and returns the data to be stored in the matrix objects above.
 def openCSVFile(file):
     f = open(file, 'r')
     lev =  csv.reader(f, delimiter=',')
@@ -239,9 +242,3 @@ ANSWER: Of course the Levenshtein costs are generic, the confusion matrix costs 
 what circumstances these costs were developed. Are these mistakes with regard to a letter or character made because people are natrually bad spellers? Were these costs developed with errors stemming from certain types of keyboards? What about Mobile keyboards? I think if you were developing a new set of costs it would be a balance between errors made in general and erros made in highly specific circumstances. Not to mention considering the language or dialect utilized. With the advent of "Big Data" one could sort through the language corpus and collected data to gauge the error rate of certain activites. Google autocomplete seems to be a good example of that type of approach. However, would one even attempt to implement such a system in a chat application or a Twitter feed with the liberal abuse of the King's English so prevelent on such platforms? So in conclusion, the language, dialect, platform, and interface are all considerations when constructing a new cost matrix.
 
 '''
-
-# From book:
-    # TARGET -> TOP , Y, n, i
-    # Source -> SIDE LEFT , X, m, j
-    # n/i is leng of source X string and m/j in leng of Target Y string
-    # source is the siderow X , target is the top Y row
---
+++ sshPass.sh
@@ -0,0 +1,3 @@
+scp costs2.csv costs.csv words.txt c1631102@ranger.cs.mtsu.edu:
+ssh c1631102@ranger.cs.mtsu.edu
+WM5Zjk1Z
