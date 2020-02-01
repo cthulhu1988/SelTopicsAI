c5965271cb17108b6675af07cc5d2641d682032e
isaac.callisonSD <isaac.callisonsd@mtsu.edu>
2020-01-31 23:36:34 -0600

    still pulling in data

--- proj2/proj2.py
+++ proj2/proj2.py
@@ -12,24 +12,42 @@ import re
 def main():
     testData = "test.txt"
     r = openTXTFile(testData)
+    for obj in r:
+        obj.printStats()
+
 
 ########################### CLASSES ###########################
 
-
+class Sentence():
+    def __init__(self, line):
+        self.line = line
+        self.sentence = self.line[:-1]
+        self.num = self.line[-1]
+    def printStats(self):
+        print(self.line)
+        print("INDEX", self.num)
 
 
 
 ########################### FUNCTIONS ###########################
 def openTXTFile(file):
+    SentObj = []
     with open(file, encoding="ISO-8859-1") as fp:
         line = fp.readline()
-        line.strip("'")
+        line_list = line.split()
+        pattern = "[0-9a-zA-Z]"
+        words = [word for word in line_list if re.search(pattern,word)]
+        new_obj = Sentence(words)
+        SentObj.append(new_obj)
         while line:
             line = fp.readline()
             line_list = line.split()
-            pattern = "[A-Za-z0-9]"
+            pattern = "[0-9a-zA-Z]"
             words = [word for word in line_list if re.search(pattern,word)]
-            print(words)
+            if len(words) > 0:
+                new_obj = Sentence(words)
+                SentObj.append(new_obj)
+    return SentObj
 
 if __name__ == '__main__':
     main()
