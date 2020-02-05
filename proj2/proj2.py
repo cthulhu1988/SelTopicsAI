f4bbb7e61d9fdc83c5a53baa83629d935c33c38d
Isaac.CallisonSD <isaacgcallison@protonmail.com>
2020-02-05 11:52:00 -0600

    getting close

--- proj2/proj2.py
+++ proj2/proj2.py
@@ -9,7 +9,6 @@ import operator
 from timeit import default_timer as timer
 import time
 from datetime import timedelta
-import multiprocessing
 def main():
     # Hardcoded file handles
     trainData = "train.txt"
@@ -26,99 +25,42 @@ def main():
     global biCounter
     biCounter = [0,0,0,0,0]
     triCounter = [0,0,0,0,0]
-    #### Get a total of some specific word ####
-    #print(getTotalCount.WordCount("."))
+
     # create ngrams for the testing data set.
     trainNgramCount = Ngrams(listOfTrainSentenceObjs)
 
     # passing in testing sentences and NGRAMS created during training.
-    #runThroughData(listOfTestingSentenceObjs, trainNgramCount)
-    # for i in listOfTestingSentenceObjs:
-    #     runThroughData(i, trainNgramCount)
-
     start_time = time.clock()
-    p1 = multiprocessing.Process(target=bigramCalc, args=[testData, trainNgramCount, biCounter])
-    p2 = multiprocessing.Process(target=trigramCalc, args=[testData, trainNgramCount, triCounter])
-
-    p1.start()
-    p2.start()
-
-    p1.join()
-    p2.join()
-    print(triCounter)
-    print(biCounter)
-
+    triCounter = trigramCalc(testData, trainNgramCount, triCounter)
     print(time.clock() - start_time, "seconds")
-    outputDataToConsole(getTotalWordCount, trainNgramCount)
+
+    outputDataToConsole(getTotalWordCount, trainNgramCount, triCounter)
 
 ########################### CLASSES ###########################
 
-def bigramCalc(data, trainNgramCount,biCounter):
-    start = "<s>"; end = "</s>"
-    #biCounter = [0,0,0,0,0]
-    with open(data, encoding="ISO-8859-1") as fp:
-        line = fp.readline()
-        while line:
-            biCounter[4]+=1
-            #start_time = time.clock()
-            words = line.split()
-            if len(words) > 0:
-                num = int(words[-1])
-                sentence = words[:-1]
-                targetWord = sentence[num]
-
-                w_1 = sentence[num-1] if num >=1 else ""
-                w_2 = sentence[num-2] if num >=2 else ""
-                sentence.insert(0,start)
-                sentence.append(end)
-                bigramDict = {kk:vv for (kk,vv) in trainNgramCount.bigramDict.items() if (kk[0] == w_1)  }
-                bigram_list = []
-
-                for key, value in bigramDict.items():
-                    bigram_list.append((key[0],key[1], value))
-                    bigram_list.sort(key=operator.itemgetter(2), reverse=True)
-
-
-                bigram_list = bigram_list[0:10] if len(bigram_list) >=10 else bigram_list[0:len(bigram_list)]
-                for idx, i in enumerate(bigram_list):
-                    #print(i)
-                    tupleTarget, guess, percentage = i
-                    if guess == targetWord:
-                        if idx == 0:
-                            biCounter[0]+=1
-                        if idx <= 3:
-                            biCounter[1]+=1
-                        if idx <= 5:
-                            biCounter[2]+=1
-                        if idx <= 10:
-                            biCounter[3]+=1
-
-            line = fp.readline()
-    return biCounter
-
-
-
 def trigramCalc(data, trainNgramCount, triCounter):
     start = "<s>"; end = "</s>"
-    #triCounter = [0,0,0,0,0]
+    triCounter = [0,0,0,0,0]
     with open(data, encoding="ISO-8859-1") as fp:
         line = fp.readline()
         while line:
             triCounter[4]+=1
             words = line.split()
             if len(words) > 0:
+                # perform operations on the strings:
                 num = int(words[-1])
                 sentence = words[:-1]
                 targetWord = sentence[num]
-
                 w_1 = sentence[num-1] if num >=1 else ""
                 w_2 = sentence[num-2] if num >=2 else ""
                 sentence.insert(0,start)
                 sentence.append(end)
-                #bigramDict = dict([ (k,r) for k,r in trainNgramCount.bigramDict.items() if k[0] == w_1 ])
+
+                # create dictionaries and lists:
+                # Turn dictionary into list, sort by highest, cut of list at 10, set a bool to avoid double counting:
+                found_match = False
                 trigramDict = {k:v for (k,v) in trainNgramCount.trigramDict.items() if (k[0] == w_2 and k[1] == w_1)  }
                 trigram_list = []
-
                 for key, value in trigramDict.items():
                     trigram_list.append((key[0],key[1],key[2], value))
                     trigram_list.sort(key=operator.itemgetter(3), reverse=True)
@@ -127,6 +69,7 @@ def trigramCalc(data, trainNgramCount, triCounter):
                 for id, y in enumerate(trigram_list):
                     t,tupleTarget, guess, percentage = y
                     if guess == targetWord:
+                        found_match = True
                         if id == 0:
                             triCounter[0]+=1
                         if id <= 3:
@@ -135,16 +78,45 @@ def trigramCalc(data, trainNgramCount, triCounter):
                             triCounter[2]+=1
                         if id <= 10:
                             triCounter[3]+=1
+                        #print("IN TRI TRI TRRI COUNTER {}".format(triCounter))
 
+
+                # create dictionaries and lists:
+                # Turn dictionary into list, sort by highest, cut off list at 10, set a bool to avoid double counting:
+                # if we did not find a match in the previous trigram section we try bigrams. Best run time is 6 minutes on this.
+                if found_match == False:
+                    bigram_list = []
+                    bigramDict = {k:v for (k,v) in trainNgramCount.bigramDict.items() if (k[0] == w_1)  }
+                    for key, value in bigramDict.items():
+                        bigram_list.append((key[0],key[1], value))
+                        bigram_list.sort(key=operator.itemgetter(2), reverse=True)
+
+                    bigram_list = bigram_list[0:10] if len(bigram_list) >=10 else bigram_list[0:len(bigram_list)]
+                    for idx, i in enumerate(bigram_list):
+                        tupleTarget, guess, percentage = i
+                        if guess == targetWord:
+                            if idx == 0:
+                                triCounter[0]+=1
+                            if idx <= 3:
+                                triCounter[1]+=1
+                            if idx <= 5:
+                                triCounter[2]+=1
+                            if idx <= 10:
+                                triCounter[3]+=1
+                            #print("IN BI COUNTER {}".format(triCounter))
+                else:
+                    pass
+            # readline to start process over
             line = fp.readline()
     return triCounter
 
+# This class creates a word dictionary from all the setences pulled into the program.
 class CountWords():
     def __init__(self, SentObj):
         self.SentObj = SentObj
         self.wordDict = {}
         self.gWCnt(self.SentObj)
-
+    # this method returns the totoal
     def WordCount(self,word):
         for k,v in self.wordDict.items():
             if k == word:
@@ -166,7 +138,8 @@ class Ngrams():
         # split the list into bigrams and trigrams
         for item in self.SentObjList:
             self.bigramList.append(self.splitIntoBigram(item.returnSentence()))
-            self.trigramList.append(self.splitIntoTrigram(item.returnSentence()))
+            self.trigramList.append(self.splitIntoTrigram(item.returnTriSentence()))
+        # stores number of each type.
         self.dictionaryCount(self.bigramList, self.bigramDict)
         self.dictionaryCount(self.trigramList, self.trigramDict)
         # number of bigrams and trigrams in each dictionary.
@@ -215,7 +188,7 @@ class Sentence():
 
         self.sentence.insert(0,self.start)
         self.sentence.append(self.end)
-    # generaic print stats function I put in a lot of my classes to make sure the program
+    # generic print stats function I put in a lot of my classes to make sure the program
     # is manipulating data correctly.
     def printStats(self):
         print(self.targetWord)
@@ -224,14 +197,24 @@ class Sentence():
         print()
     def returnSentence(self):
         return self.sentence
-
+    def returnTriSentence(self):
+        triSetence = self.sentence
+        triSetence.insert(0,self.start)
+        triSetence.append(self.end)
+        return triSetence
 
 ########################### FUNCTIONS ###########################
-def outputDataToConsole(getTotalWordCount, trainNgramCount):
+def outputDataToConsole(getTotalWordCount, trainNgramCount, triCounter):
     print("Unique unigrams extracted: {}".format(getTotalWordCount.count))
     print("Unique bigrams extracted: {}".format(trainNgramCount.bigramNum))
     print("Unique trigrams extracted: {}".format(trainNgramCount.trigramNum))
+    outputSentence(1, triCounter[0], triCounter[4])
+    outputSentence(3, triCounter[1], triCounter[4])
+    outputSentence(5, triCounter[2], triCounter[4])
+    outputSentence(10, triCounter[3], triCounter[4])
 
+def outputSentence(a,b,c):
+    print("# of times correct word found in top {} highest probability n-grams {} of {} predictions".format(a,b,c))
 
 def openTXTFile(file, bool):
     SentObj = []
@@ -241,8 +224,6 @@ def openTXTFile(file, bool):
         if len(words) > 0:
             new_obj = Sentence(words, bool)
             SentObj.append(new_obj)
-        # pattern = "[0-9a-zA-Z]"
-        # words = [word for word in line_list if re.search(pattern,word)]
         while line:
             line = fp.readline()
             words = line.split()
@@ -253,3 +234,16 @@ def openTXTFile(file, bool):
 
 if __name__ == '__main__':
     main()
+
+"""
+1. Why you believe the results are as output(ed) by your code.
+ANSWER:
+
+2. The test sentences are extracted randomly from the news stories used in the training data. A sentence is in
+either the training data or the test data, but not both. Discuss the pros and cons of this approach.
+ANSWER:
+
+3. What, in your opinion, is a better way to split the data into training and test components? Give reasons for
+your answer.
+ANSWER:
+"""
